1aa9401f23ec4d27c09149b51e9340290bfd5360
= <=>
2021-09-27 21:17:07 +0700

    ToDoLisst
    ToDoLisst

---
+++ ToDoList _NguyenHai/Jquery/site-jquery.js
@@ -0,0 +1,40 @@
+function addTodoItem() {
+  var todoItem = $("#new-todo-item").val();
+  $("#todo-list").append("<li><input type='checkbox'" + 
+                         " name='todo-item-done'" + 
+                         " class='todo-item-done'"+ 
+                         " value='" + todoItem + "' /> " + 
+                         todoItem +
+                         " <button class='todo-item-delete'>"+
+                         "Delete</button></li>");
+  
+ $("#new-todo-item").val("");
+}
+
+function deleteTodoItem(e, item) {
+  e.preventDefault();
+  $(item).parent().fadeOut('2000', function() { 
+    $(item).parent().remove();
+  });
+}
+
+                           
+
+$(function() {
+ 
+   $("#add-todo-item").on('click', function(e){
+     e.preventDefault();
+     addTodoItem()
+   });
+  
+
+  $("#todo-list").on('click', '.todo-item-delete', function(e){
+    var item = this; 
+    deleteTodoItem(e, item)
+  })
+  
+  $(document).on('click', ".todo-item-done", function completeTodoItem() {  
+  $(this).parent().toggleClass("strike");
+})
+
+});
